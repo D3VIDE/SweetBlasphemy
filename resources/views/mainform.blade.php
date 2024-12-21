a71fd0e4c98a3505706251577ee823b10b17a3b8
D3VIDE <errandojose03@gmail.com>
2024-12-21 18:13:32 +0700

    Add: ico, image logo

---
+++ resources/views/mainform.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sweet Blasphemy</title>
+  <link rel="icon" href="/images/Logo SB.ico"type="image/x-icon"/>
+</head>
+</head>
+<body>
+  <p>halo</p>
+</body>
+</html>
