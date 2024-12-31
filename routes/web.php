ef9e27f5a19c0e8abfd4ea649f862731efc64070
D3VIDE <errandojose03@gmail.com>
2024-12-31 16:16:56 +0700

    add navbar and font

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layout/mainform');
+    return view('layout/mainform',[
+        "title" => "SB | Home"
+    ]);
 });
