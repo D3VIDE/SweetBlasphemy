15124a636e1496de5151f3dfdb585cd6869c41de
D3VIDE <errandojose03@gmail.com>
2025-01-04 22:07:19 +0700

    Updating: font

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg">
   <div class="container">
-    <a class="navbar-brand custom-font" href="#">Sweet Blasphemy</a>
+    <a class="navbar-brand text-trajanus" href="#"><span style="font-size:1.7rem; padding:0px;margin:0px;">s</span>weet <span style="font-size:1.7rem; padding:0px;margin:0px;">b</span>lasphemy</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto custom-normal">
+      <ul class="navbar-nav ms-auto custom-normal" style="padding:0 50px;">
         <li class="nav-item">
           <a class="nav-link" href="#">Home</a>
         </li>
